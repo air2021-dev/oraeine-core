98e9a8ab839a9ba8a2e25c702eacd2cedb819952
Potahog <potahog@gmail.com>
2025-05-14 02:05:52 +0900

    chore: Add .gitignore and initial project setup

---
+++ src/main/kotlin/com/olmaru/oraein/core/OraeinCoreApplication.kt
@@ -0,0 +1,11 @@
+package com.olmaru.oraein.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OraeinCoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<OraeinCoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/olmaru/oraein/core/OraeinCoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.olmaru.oraein.core
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OraeinCoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
